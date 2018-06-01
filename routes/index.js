1bc20e8ea3bf9f85096e21a4cc626eab7a01a6f9
pyagais <pyagais@mail.ru>
2018-06-02 01:26:40 +0600

    almost done

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,57 @@
 var express = require('express');
 var router = express.Router();
+var users = []
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
 router.get('/login', function(req, res, next) {
+
+  res.render('login', {});
+});
+
+
+router.post('/login', function(req, res, next) {
+	let check = false
+	for(let i = 0; i < users.length; i++){
+		if (req.body.login == users[i].name & req.body.pass == users[i].password) {
+			check = true
+		}
+	}
+	if (check == true ) {
+		console.log('succes')
+	} else if(check == false){
+		console.log('login or password incorrect')
+	}
+
   res.render('login', {});
 });
 
 router.get('/register', function(req, res, next) {
+  res.render('register', {});
+});
+
+
+router.post('/register', function(req, res, next) {
+	let check = false
+	for(let i = 0; i < users.length; i++){
+		if (req.body.login == users[i].name) {
+			check = true
+		}
+	}
+
+	if (check == false) {
+	users.push({
+		name:req.body.login,
+		password:req.body.pass
+	})
+	}else if (check == true) {
+		console.log('Nickname already in use, try a different one')
+	}
+	console.log(users)
   res.render('register', {});
 });
 
