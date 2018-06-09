e77d1a9ee936ef4df1319f17f3cb73c64c884454
pyagais <pyagais@mail.ru>
2018-06-09 15:12:30 +0600

    homework

--- routes/index.js
+++ routes/index.js
@@ -1,60 +1,107 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var async = require('async')
 var users = []
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
+router.get('/', function(req, res, next) {
+	res.render('index', { message: null });
+});
 
 router.get('/login', function(req, res, next) {
-
-  res.render('login', {});
-});
-
-
-router.post('/login', function(req, res, next) {
-	var message = 'succes'
-	let check = false
-	for(let i = 0; i < users.length; i++){
-		if (req.body.login == users[i].name && req.body.pass == users[i].password) {
-			check = true
-			break
-			res.send('this user already exist')
-		}
-	}
-	res.send('you are logged in')
-  res.render('login', {});
+	res.render('login', {});
 });
 
 router.get('/register', function(req, res, next) {
-  res.render('register', {});
+	res.render('register', {});
 });
-
 
 router.post('/register', function(req, res, next) {
-	let check = false
-	for(let i = 0; i < users.length; i++){
-		if (req.body.login == users[i].name) {
-			check = true
+	let found = false
+	for(let i = 0; i < users.length; i++) {
+		if(users[i].login == req.body.login) {
+			found = true
+			res.render('index', {message: 'This user exist'})
 		}
 	}
-
-	if (check == false) {
-	users.push({
-		name:req.body.login,
-		password:req.body.pass
-	})
-	}else if (check == true) {
-		res.send('Nickname already in use, try a different one')
+	if(!found) { //found == false
+		users.push({
+			login: req.body.login,
+			pass: req.body.pass
+		})
+		res.render('index', {message: 'You are registered'})
 	}
-	console.log(users)
-  res.render('register', {});
 });
 
-router.get('/main', function(req, res, next) {
-  res.render('main', {});
+router.post('/login', function(req, res, next) {
+	let found = false
+	for(let i = 0; i < users.length; i++) {
+		if(users[i].login == req.body.login && users[i].pass == req.body.pass) {
+			found = true
+			res.render('index', {message: 'You are logged in!'})
+		}
+	}
+	if(!found) { //found == false
+		res.render('index', {message: 'This user is not exists'})
+	}
+});
+
+router.get('/get/data', function(req, res, next) {
+	var query = 'http://catalog.api.2gis.ru/2.0/catalog/branch/search?locale=ru_KG&region_id=112&q=%D1%82%D0%B5%D0%B0%D1%82%D1%80&region_id=1&page=*page*&page_size=1&fields=items.reviews&format=json&key=ruoedw9225';
+	//page=*page*
+	var names = []
+	var ids = []
+	
+	for(let i = 1;i <= 12; i++) {
+		ids.push(i)
+	}
+
+	async.eachSeries(ids, function iterator(id, callback) {
+		var axilary = query.replace('*page*', id)
+		request(axilary, function (error, response, body) { 
+			var obj = JSON.parse(body)
+			var name = obj.result.items[0].name
+			names.push(name)
+			callback()
+		});
+	}, function done() {
+		res.send(names)
+	});
+});
+
+router.get('/form', function(req, res, next) {
+	res.render('form', {});
+});
+
+router.post('/form', function(req, res, next) {
+	var query = 'http://catalog.api.2gis.ru/2.0/catalog/branch/search?locale=ru_KG&region_id=112&q=*some_place*&region_id=1&page=*page*&page_size=1&fields=items.reviews&format=json&key=ruoedw9225';
+	var names = []
+	var ids = []
+	var place = []
+	
+
+	for(let i = 1;i <= 10; i++) {
+		ids.push(i)
+	}
+	place.push(req.body.place)
+	async.eachSeries(ids, function iterator(id, callback) {
+		var axilary = query.replace('*some_place*', place[0])
+		axilary = query.replace('*page*', id)
+			console.log(place[0])
+			console.log(id)
+			console.log(axilary)
+		request(axilary, function (error, response, body) { 
+			var obj = JSON.parse(body)
+			var name = obj.result.items[0].name
+			names.push(name)
+			callback()
+		});
+	}, function done() {
+		res.send(names)
+	});
 });
 
 module.exports = router;
+
+
